d121932a2d588fefaae61d03a9fa142dd3adbe41
Ryan Dens <ryandens735@gmail.com>
2020-06-07 10:29:56 -0400

    :heavy_plus_sign: configure root gradle script for Java and with formatter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,27 @@
-subprojects {
+plugins {
+    id("com.diffplug.gradle.spotless") version "3.28.1"
+}
+
+allprojects {
+    apply(plugin = "com.diffplug.gradle.spotless")
     apply<JavaPlugin>()
+    spotless {
+        java {
+            removeUnusedImports()
+            googleJavaFormat()
+        }
+        kotlinGradle {
+            ktlint()
+        }
+    }
+
+    configure<JavaPluginConvention> {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+subprojects {
     repositories {
         mavenCentral()
     }
